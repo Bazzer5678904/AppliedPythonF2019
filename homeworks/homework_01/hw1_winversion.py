97ace446ac939906e907e7d1c5691b714a0f0840
Kirill <kirill.lego00@mail.ru>
2019-09-22 21:14:03 +0300

    upd

--- homeworks/homework_01/hw1_winversion.py
+++ homeworks/homework_01/hw1_winversion.py
@@ -8,11 +8,11 @@ def word_inversion(string):
     for i in range(len(string)):
         if string[i] == " ":
             k = i - 1
-            for j in range(begin, (i+begin)//2):
+            for j in range(begin, (i + begin)//2):
                 string[j], string[k] = string[k], string[j]
                 k = k - 1
             begin = i + 1
-        if i == len(string) - 1:
+        if i == (len(string) - 1):
             k = i
             for j in range(begin, (i + begin) // 2):
                 string[j], string[k] = string[k], string[j]
